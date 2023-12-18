73f10a55e7c839d20fe0249a9e6bd78184ff983e
Baibhav Gautam <baibhavgautam@Baibhavs-MacBook-Pro.local>
2023-12-17 23:07:16 -0800

    Handle edge cases

--- src/parser/cssParser.js
+++ src/parser/cssParser.js
@@ -1,49 +1,30 @@
-/**
- * Parses a raw CSS string and returns an object representing the styles.
- * @param {string} rawCss - The raw CSS content.
- * @returns {Object} - Parsed CSS as an object.
- */
 function parseCss(rawCss) {
-    const styleObjectBySelector = {};
-    let currentSelector = '';
-    let currentProperty = '';
-  
-    const processChar = (char, accumulatedChars) => {
-      switch (char) {
-        case '{':
-          currentSelector = accumulatedChars.trim();
-          styleObjectBySelector[currentSelector] = {};
-          break;
-        case ':':
-          currentProperty = accumulatedChars.trim();
-          break;
-        case ';':
-          if (currentSelector && currentProperty) {
-            styleObjectBySelector[currentSelector][currentProperty] = accumulatedChars.trim();
-          }
-          currentProperty = '';
-          break;
-        case '}':
-          currentSelector = '';
-          currentProperty = '';
-          break;
-        default:
-          console.warn(`Encountered unexpected character: ${char}`);
-      }
-    };
-  
-    let accumulatedChars = '';
-    for (const char of rawCss) {
-      if (['{', ':', ';', '}'].includes(char)) {
-        processChar(char, accumulatedChars);
-        accumulatedChars = '';
-      } else {
-        accumulatedChars += char;
-      }
+  const styleObjectBySelector = {};
+  let currentSelector = '';
+
+  const addPropertiesToSelector = (selector, properties) => {
+    if (!styleObjectBySelector[selector]) {
+      styleObjectBySelector[selector] = {};
     }
-  
-    return styleObjectBySelector;
-  }
-  
-  module.exports = { parseCss };
-  
+
+    properties.split(';').forEach(property => {
+      const [key, value] = property.split(':');
+      if (key && value) {
+        styleObjectBySelector[selector][key.trim()] = value.trim();
+      }
+    });
+  };
+
+  rawCss.split('}').forEach(rule => {
+    const parts = rule.split('{');
+    if (parts.length === 2) {
+      currentSelector = parts[0].trim();
+      const propertyBuffer = parts[1].trim();
+      addPropertiesToSelector(currentSelector, propertyBuffer);
+    }
+  });
+
+  return styleObjectBySelector;
+}
+
+module.exports = { parseCss };
